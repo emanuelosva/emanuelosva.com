3fc893cb8839bfff36759958f7d392bb4645123a
emanuelosva <emanuelosva@gmail.com>
2020-08-03 10:23:25 -0500

    First header

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,30 +3,87 @@
 </script>
 
 <style>
+  @media screen and (min-width: 768px) {
+  }
+  .Nav {
+    position: absolute;
+    left: calc(100vw - 120px);
+    top: 30%;
+    z-index: 1000;
+  }
 
+  .Nav ul {
+    list-style: none;
+  }
+
+  .Nav ul li {
+    box-sizing: border-box;
+    width: 50px;
+    height: 50px;
+    margin: 0 0 0.8em 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    background-color: #2b2a2a;
+    border-radius: 25px;
+  }
+
+  .Nav ul li a {
+    display: flex;
+    text-decoration: none;
+  }
+
+  .Nav ul li a i {
+    position: relative;
+    right: 0px;
+    z-index: 2000;
+    font-size: 1.2em;
+    color: #ffffff;
+  }
+
+  [aria-current] i {
+    color: orange;
+  }
 </style>
 
-<nav>
+<nav class="Nav">
   <ul>
     <li>
-      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-        home
+      <a aria-current={segment === '.' ? 'page' : undefined} href=".">
+        <i class="fa fa-home" />
       </a>
     </li>
-    <li>
-      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-        about
-      </a>
-    </li>
-
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
     <li>
       <a
+        aria-current={segment === 'about' ? 'page' : undefined}
         rel="prefetch"
+        href="about">
+        <i class="fa fa-user" />
+      </a>
+    </li>
+    <li>
+      <a
+        aria-current={segment === 'portfolio' ? 'page' : undefined}
+        rel="prefetch"
+        href="portfolio">
+        <i class="fa fa-briefcase" />
+      </a>
+    </li>
+    <li>
+      <a
+        aria-current={segment === 'contact' ? 'page' : undefined}
+        rel="prefetch"
+        href="contact">
+        <i class="fa fa-envelope" />
+      </a>
+    </li>
+    <li>
+      <a
         aria-current={segment === 'blog' ? 'page' : undefined}
+        rel="prefetch"
         href="blog">
-        blog
+        <i class="fas fa-blog" />
       </a>
     </li>
   </ul>
