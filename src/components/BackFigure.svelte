81ee266bea4ee327d12a37b2c64a81cbe24abb0b
emanuelosva <emanuelosva@gmail.com>
2020-08-03 10:33:14 -0500

    Background figure

---
+++ src/components/BackFigure.svelte
@@ -0,0 +1,17 @@
+<style>
+  @media screen and (min-width: 769px) {
+    .BackFigure {
+      position: fixed;
+      left: -83%;
+      top: -50%;
+      z-index: -1;
+      height: 200%;
+      width: 100%;
+      display: block;
+      background-color: #eb7423;
+      transform: rotate(-15deg);
+    }
+  }
+</style>
+
+<div class="BackFigure" />
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,11 @@
 <script>
   import Nav from "../components/Nav.svelte";
+  import BackFigure from "../components/BackFigure.svelte";
   export let segment;
 </script>
 
 <Nav {segment} />
+<BackFigure />
 
 <main>
   <slot />
